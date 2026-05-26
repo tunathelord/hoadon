0bd0a4e1ed3088b721a1def99ad542ad01c8d393
tuna <trantrungnam2006@gmail.com>
2026-05-27 01:41:25 +0700

    test

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'shop-tuna-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+// Cài đặt Service Worker và lưu trữ tài nguyên vào Cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Kích hoạt SW và xóa cache cũ nếu có thay đổi phiên bản
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Trả dữ liệu từ Cache khi mất mạng (Offline mode)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
